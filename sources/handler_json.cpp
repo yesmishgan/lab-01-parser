d4b4b97df1c2082e859baca0ab4bc09e44663716
4Rjee <dolbnin@protonmail.com>
2020-09-22 11:10:05 +0300

    C

--- sources/handler_json.cpp
+++ sources/handler_json.cpp
@@ -17,7 +17,7 @@ handler_json::handler_json(const std::string &jsonPath) {
         if (!file) {
             throw std::out_of_range{"unable to open json: " + jsonPath};
         }
-        if(!(file >> data)){
+        if (!(file >> data)){
             throw std::out_of_range("Incorrect json");
         }
     }else{
